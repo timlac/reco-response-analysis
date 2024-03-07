5350d7a56d375d60d0eeb15156b4ea08770c7b1a
timlac <tim@lachmann.nu>
2024-03-07 15:50:44 +0100

    continued work on reliability

---
+++ appraisal/reliability.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import pingouin as pg
+
+path = "../data/appraisal/export_appraisal.csv"
+df = pd.read_csv(path)
+
+
+def find_mean_square_between():
+    pass
